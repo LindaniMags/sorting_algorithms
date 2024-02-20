d4a3714273fe5e670c05e8a82efc65ba49e5d61c
LindaniMags <lindanimags@gmail.com>
2024-02-20 11:03:24 -0800

    0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "sort.h"
 
 /**
@@ -28,7 +29,6 @@ void bubble_sort(int *array, size_t size)
 			if (array[j] > array[j + 1])
 			{
 				int temp = array[j];
-
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
 				print_array(array, size);

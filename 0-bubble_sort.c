f0448260aef271e1416f01fc6b96008a7001c64c
LindaniMags <lindanimags@gmail.com>
2024-02-20 12:03:35 -0800

    0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -16,7 +16,9 @@ void bubble_sort(int *array, size_t size)
 		{
 			if (array[j] > array[j + 1])
 			{
-				int temp = array[j];
+				int temp;
+
+				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
 				print_array(array, size);

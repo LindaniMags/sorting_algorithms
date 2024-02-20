c45fc84048b4e747eaf5e4b2969e052471b00d52
LindaniMags <lindanimags@gmail.com>
2024-02-20 11:58:47 -0800

    0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,29 +2,17 @@
 #include "sort.h"
 
 /**
- * print_array - Prints an array
- * @array: Array of integers
- * @size: Size of array
- */
-void print_array(int *array, size_t size)
-{
-	for (size_t i = 0; i < size; i++)
-	{
-		printf("%d ", array[i]);
-	}
-	printf("\n");
-}
-
-/**
  * bubble_sort -  Sorts an array of integers in ascending order
  * @array: Array of integers
  * @size: Size of array
  */
 void bubble_sort(int *array, size_t size)
 {
-	for (size_t i = 0; i < size - 1; i++)
+	size_t i, j;
+
+	for (i = 0; i < size - 1; i++)
 	{
-		for (size_t j = 0; j < size - i - 1; j++)
+		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
